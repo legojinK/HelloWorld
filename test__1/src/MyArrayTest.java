ceaa7bb04f209f7892ed35b1fad2603fb510fdaa
legojinK <ckgksck@gmail.com>
2022-02-27 18:36:26 +0900

    ArrayTest

---
+++ test__1/src/MyArrayTest.java
@@ -0,0 +1,13 @@
+
+public class MyArrayTest{
+public static void main(String[] args) {
+
+
+        MyArray myArray = new MyArray();
+        myArray.initMyArray(2);
+        myArray.printArray();
+        }
+ }
+
+
+
