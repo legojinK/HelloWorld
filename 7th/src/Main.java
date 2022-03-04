41baef1e73a324efa17969b69bdb7370956c18ed
legojinK <ckgksck@gmail.com>
2022-03-04 11:13:00 +0900

    [coding Test] 10952번

--- 7th/src/Main.java
+++ 7th/src/Main.java
@@ -1,11 +1,22 @@
 import java.util.Scanner;
 
+// 백준 10952번
 public class Main {
     public static void main(String[] args) {
+
+
         Scanner sc = new Scanner(System.in);
 
+        while(true){
+            int A = sc.nextInt();
+            int B = sc.nextInt();
 
-
+            if(A==0 && B==0){
+                sc.close();
+                break;
+            }
+            System.out.println(A+B);
+        }
 
     }
 }
